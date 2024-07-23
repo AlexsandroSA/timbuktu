d621b08c3c4522c56cf31576ae6e975039e50402
Alexsandro Silva Alves <alexsando.s.alves@gmail.com>
2024-07-23 07:43:14 -0300

    refactor: add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import styles from "./Home.module.css";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Timbuktu</h1>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Timbuktu</h1>
 
-      <q>
-        O sal vem do norte, o ouro vem do sul, mas as palavras de Deus e os
-        tesouros da sabedoria vêm de Timbuktu
-      </q>
-    </main>
+        <q className={styles.description}>
+          O sal vem do norte, o ouro vem do sul, mas as palavras de Deus e os
+          tesouros da sabedoria vêm de Timbuktu
+        </q>
+      </main>
+    </div>
   );
 }
