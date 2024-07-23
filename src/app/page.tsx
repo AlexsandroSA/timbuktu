ec1ac2cc0ec1344b9ba7f7a5a5b9f078435ab9cc
Alexsandro Silva Alves <alexsando.s.alves@gmail.com>
2024-07-23 07:35:30 -0300

    chore: update to next 14

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Timbuktu</h1>
+
+      <q>
+        O sal vem do norte, o ouro vem do sul, mas as palavras de Deus e os
+        tesouros da sabedoria vêm de Timbuktu
+      </q>
+    </main>
+  );
+}
